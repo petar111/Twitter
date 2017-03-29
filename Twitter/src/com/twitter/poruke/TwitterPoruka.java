f7c0a04d1f04d10181594373638c291bf66340cf
petar111 <jpetar765@gmail.com>
2017-03-29 21:07:42 +0200

    Napravljen doc folder i uveden novi source folder test
    Napravljen doc folder koji sadrzi dokumentaciju(opis svih elemenata
    preko html fajla) projekta. Takodje je napravljen i novi source folder
    test.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,30 +1,71 @@
 package com.twitter.poruke;
-
+/**
+ * Klasa TwitterPoruka koja generise poruku i pokazuje
+ * koji korisnik je generisao tu poruku.
+ * @author Petar Jeremic
+ *
+ */
 public class TwitterPoruka {
+	
+	/**
+	 * Ime korisnika koji generise poruku.
+	 */
 	private String korisnik;
+	/**
+	 * Poruka koju je korisnik generisao.
+	 */
 	private String poruka;
-
+	
+	/**
+	 * Vraca ime korisnika kao String
+	 * @return ime korisnika
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
-
+	
+	/**
+	 * Daje ime korisniku.
+	 * @param korisnik ime korisnika
+	 * @throws RuntimeException za vrednosti:
+	 * <ul>
+	 * <li>NULL</li>
+	 * <li>Prazan String</li>
+	 * </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || !korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
-
+	/**
+	 * Vraca poruku koja se salje.
+	 * @return poruka koja se salje
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+	/**
+	 * Generise poruku koja se salje.
+	 * @param poruka poruka koja se salje
+	 * @throws RuntimeException za vrednosti:
+	 * <ul>
+	 * <li>NULL</li>
+	 * <li>Prazan String</li>
+	 * <li>String koji sadrzi vise od 140 karaktera</li>
+	 * </ul>
+	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-
+	/**
+	 * Vraca informacije o objektu odredjenom formatu.
+	 * @return KORISNIK: ###  PORUKA: ###
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
+
 }
